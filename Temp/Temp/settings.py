2255845e082ca9465f7ee10a0386d9c5715a417e
Shubhansu kumar <shubhansu2021@gmail.com>
2025-01-15 11:43:52 +0530

    Moved Secret key to System ENV

--- Temp/Temp/settings.py
+++ Temp/Temp/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+# Load environment variables from .env
+load_dotenv()  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&5z0t!+3pk$sqx9d-*yk8wlv35skqx57e#l@)p9z4)7)p&h9t'
+# SECRET_KEY = 'Moved secret key to system env'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
