8a3a672929e09e36d319e0a47c8fb13d851c53a8
Shubhansu kumar <shubhansu2021@gmail.com>
2025-02-26 10:27:15 +0530

    Solution 1

--- SampleQuestions/00_Question1/Solution/library/urls.py
+++ SampleQuestions/00_Question1/Solution/library/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    re_path(r'^author/(?P<author_name>\w+)/$', views.search_by_author, name='author'),
+    re_path(r'^author/(?P<author_name>\w+)/(?P<year>\d+)/$', views.search_by_author_year, name='year'),
 ]
--- SampleQuestions/00_Question1/Solution/library/views.py
+++ SampleQuestions/00_Question1/Solution/library/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# Create a hardcoded dictionary of books 
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
