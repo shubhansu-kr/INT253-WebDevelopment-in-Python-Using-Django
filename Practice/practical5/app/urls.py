eb3ad019b23da1561d8cdb11e7446e12711d34f7
shubhansu-kr <shubhansu2021@gmail.com>
2025-04-29 22:13:50 +0530

    Fix: Logout

--- Practice/practical5/app/urls.py
+++ Practice/practical5/app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.userSignupView, name='signup'),
     path('login/', authViews.LoginView.as_view(template_name='app/login.html'), name='login'),
-    path('logout/', authViews.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', authViews.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboardView, name='dashboard'),
 ]
--- Practice/practical5/practical5/settings.py
+++ Practice/practical5/practical5/settings.py
@@ -15,9 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-LOGIN_URL = '/app/login/'
-LOGIN_REDIRECT_URL = '/app/dashboard/'
-LOGOUT_REDIRECT_URL = '/app/login/'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'practical5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
